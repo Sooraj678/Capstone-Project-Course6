fc03ffefa74e44ea4fa666fd6b3522218ea5e54e
Suraj Singh <surajsingh9090@gmail.com>
2022-07-06 15:59:07 +0530

    Develop JPA Entity 2

--- foodbox-ecommerce/src/main/java/com/foodbox/entity/Product.java
+++ foodbox-ecommerce/src/main/java/com/foodbox/entity/Product.java
@@ -1,6 +1,10 @@
 package com.foodbox.entity;
 
 import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -12,15 +16,46 @@ import lombok.Data;
 @Table(name="product")
 @Data
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+	
+	@ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+	
+	@Column(name = "sku")
 	private String sku;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
+	
+	@Column(name = "image_url")
 	private String imageUrl;
+	
+	@Column(name = "active")
 	private boolean active;
+	
+	@Column(name = "units_in_stock")
 	private int unitsInStock;
+	
+	@Column(name = "date_created")
+	@CreationTimestamp
+	/* By This annotation Hibernate will automatically manage the timestamps, No need for developer
+	 * to manually call these methods to set these fields*/
 	private Date dateCreated;
+	
+	@Column(name = "last_updated")
+	@UpdateTimestamp
+	/* By This annotation Hibernate will automatically manage the timestamps, No need for developer
+	 * to manually call these methods to set these fields*/
 	private Date lastUpdated;
 	
 
---
+++ foodbox-ecommerce/src/main/java/com/foodbox/entity/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.foodbox.entity;
+
+import java.util.Set;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product_category")
+// @Data -- Known bug
+@Getter
+@Setter
+public class ProductCategory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+	private Set<Product> products;
+
+}
