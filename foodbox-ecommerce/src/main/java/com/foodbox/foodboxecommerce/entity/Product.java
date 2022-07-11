5da08223bd66eed5a27b0c8ae4442c94fd2cb6e3
Suraj Singh <surajsingh9090@gmail.com>
2022-07-11 13:59:48 +0530

    Changes to get Proper response on APIs Call

--- foodbox-ecommerce/src/main/java/com/foodbox/foodboxecommerce/entity/Product.java
+++ foodbox-ecommerce/src/main/java/com/foodbox/foodboxecommerce/entity/Product.java
@@ -8,13 +8,13 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import lombok.Data;
+//import lombok.Data;
 /* Remember always that lombak project will create getters and setters for you
  * internally automatically, we won't be able to see these getters and setters
  * but it will work internally*/
 @Entity
 @Table(name="product")
-@Data
+//@Data
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,6 +57,102 @@ public class Product {
 	/* By This annotation Hibernate will automatically manage the timestamps, No need for developer
 	 * to manually call these methods to set these fields*/
 	private Date lastUpdated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ProductCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(ProductCategory category) {
+		this.category = category;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", category=" + category + ", sku=" + sku + ", name=" + name + ", description="
+				+ description + ", unitPrice=" + unitPrice + ", imageUrl=" + imageUrl + ", active=" + active
+				+ ", unitsInStock=" + unitsInStock + ", dateCreated=" + dateCreated + ", lastUpdated=" + lastUpdated
+				+ "]";
+	}
 	
 
 }
--- foodbox-ecommerce/src/main/java/com/foodbox/foodboxecommerce/entity/ProductCategory.java
+++ foodbox-ecommerce/src/main/java/com/foodbox/foodboxecommerce/entity/ProductCategory.java
@@ -4,14 +4,14 @@ import java.util.Set;
 
 import javax.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 
 @Entity
 @Table(name = "product_category")
 // @Data -- Known bug
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class ProductCategory {
 	
 	@Id
@@ -25,4 +25,35 @@ public class ProductCategory {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private Set<Product> products;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductCategory [id=" + id + ", categoryName=" + categoryName + ", products=" + products + "]";
+	}
+
+	
+	
 }
